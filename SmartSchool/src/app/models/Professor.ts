de7986767fb411d798eb28d080bd7945294e4565
Raphael2790 <rssshev@gmail.com>
2020-06-28 19:35:37 -0300

    Adicionado o services para professores

--- SmartSchool/src/app/models/Professor.ts
+++ SmartSchool/src/app/models/Professor.ts
@@ -1,5 +1,5 @@
 export class Professor {
   id: number;
   nome: string;
-  materia: string;
+  disciplina: string;
 }
--- SmartSchool/src/app/professores/professores.component.ts
+++ SmartSchool/src/app/professores/professores.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, TemplateRef } from '@angular/core';
 import { Professor } from '../models/Professor';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { BsModalService, BsModalRef } from 'ngx-bootstrap/modal';
+import { ProfessorService } from './professor.service';
 
 @Component({
   selector: 'app-professores',
@@ -16,34 +17,55 @@ export class ProfessoresComponent implements OnInit {
   public SimplesTexto: string;
   public modalRef: BsModalRef;
 
-  public teachers =[
-    {id:1,nome:"Miguel", materia:"Português"},
-    {id:2,nome:"João", materia:"Matemática"},
-    {id:3,nome:"Antonio", materia:"Fisica"},
-    {id:4,nome:"Hugo", materia:"História"}
-  ]
+  public professores : Professor[];
   
   openModal(template: TemplateRef<any>) {
     this.modalRef = this.modalService.show(template);
   }
 
   constructor(private fb: FormBuilder,
-              private modalService: BsModalService) { 
+              private modalService: BsModalService,
+              private professorService : ProfessorService) { 
     this.criarForm();
   }
 
   ngOnInit() {
+    this.carregarProfessores();
+  }
+
+  carregarProfessores() {
+    this.professorService.getAll().subscribe(
+      (professores: Professor[]) => {
+      this.professores = professores;
+      },
+      (error: any) => {
+        console.log(error);
+      }
+    )
   }
 
   criarForm() {
     this.TeacherForm = this.fb.group({
+      id:[""],
       nome:['', Validators.required],
-      materia:['', Validators.required]
+      //diciplina:['', Validators.required]
     })
   }
 
+  salvarProfessor(professor: Professor) {
+    this.professorService.put(professor.id, professor).subscribe(
+      (data: Professor) => { 
+        console.log(data);
+        this.carregarProfessores();
+      },
+      (error: any) => {
+        console.log(error)
+      }
+    )
+  }
+
   teacherSubmit() {
-    console.log(this.TeacherForm.value)
+    this.salvarProfessor(this.TeacherForm.value)
   }
 
   selectTeacher(teacher:Professor) {
